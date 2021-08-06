1912f1fb98b39feaf382da91b8957d9ea651b1ae
simrath randhawa <srandhawa1250@gmail.com>
2021-08-06 13:02:30 -0400

    update

---
+++ frontend/src/components/BeerReviews.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="brewery-reviews">
+    <div id = "review-details" v-for="review in allBeerReviews" v-bind:key="review.id">
+    <h2 id='reviewer-name'>{{review.reviewName}}</h2>
+    </div>
+    </div>
+</template>
+
+<script>
+import reviewService from '../services/ReviewService'
+export default {
+    name: 'beer-reviews',
+    data(){
+        return {
+            allBeerReviews: []
+        }
+    },
+    created() {
+        reviewService.getBeerReviews(this.$route.params.breweryId, this.$route.params.beerId).then((response) => {
+            this.allReviews = response.data;
+        })
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/BeerReviewDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+<div id='beer-review-list'>
+  <beer-reviews />
+  </div>
+</template>
+
+<script>
+import BeerReviews from '../components/BeerReviews'
+export default {
+    name: 'beer-review-display',
+    components: {
+        BeerReviews
+    }
+
+}
+</script>
+
+<style>
+
+</style>
