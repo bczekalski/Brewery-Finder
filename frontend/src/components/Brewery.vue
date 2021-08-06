1d8e23dcebe6d4d49b7a2c8dd2b949d630626146
Diana Aschner <dianaaschner@gmail.com>
2021-08-06 10:07:09 -0400

    merge conflicts resolved

--- frontend/src/components/Brewery.vue
+++ frontend/src/components/Brewery.vue
@@ -1,38 +1,81 @@
 <template>
-
-      <div class="brewery-list">
-        <h2 class="brewery-name">{{ breweryDetails.name }}</h2>
-        <div class="brewery-details">
-        <p class="brewery-history">{{ breweryDetails.history }}</p>
-        <p class="contact-info">{{ breweryDetails.contactInfo }}</p>
-        <p class="operation-time">{{ breweryDetails.operationTime }}</p>
-        <p class="full-address">{{ breweryDetails.address }}</p>
-        
-        
+  <div id="breweries">
+    <div class="brewery-list" v-for="brewery in allBreweries" v-bind:key="brewery.id">
+        <div class="container-breweries">
+            <h2 class="brewery-name">{{ brewery.name }}</h2>
+            <div class="brewery-details">
+                <div class="brewery-history a">History: {{ brewery.history }}</div>
+                <div class="contact-info a">Contact Information: {{ brewery.contactInfo }}</div>
+                <div class="operation-time a">Hours: {{ brewery.operationTime }}</div>
+                <div class="full-address a">
+                <div class="address-line-1">Address: {{ brewery.address }}</div>
+                <div class="address-line-2"> 
+                    </div>
+                    {{brewery.city}},  {{brewery.state}}  {{brewery.zipCode}}  </div>
+                <div><a class="brewery-website a" :href="brewery.website" target="_blank" :alt="brewery-website" >Website</a></div>
+                <div><img class="logo-image a" :src="brewery.image" alt="Brewery Logo"></div>
+            </div>
         </div>
-
-        <router-link id="review-button" v-bind:to="{ name: 'brewery-review-display', params: {breweryId: breweryDetails.id } }">Reviews</router-link>
-      </div>
+        <br><br><br>
+    </div>
+  </div>
 </template>
 
 <script>
-import breweryService from "../services/BreweryService"
+import breweryService from "../services/BreweryService.js"
 export default {
-    name: 'brewery',
+    name: "breweries",
     data() {
         return {
-            breweryDetails: {}
+            allBreweries: [],
+            website: ''
         }
         },
         created(){
-            breweryService.getById(this.$route.params.breweryId)
+            breweryService.getBreweries()
             .then(response => {
-                this.breweryDetails = response.data;
+                this.allBreweries = response.data;
             }); 
         }
     }
+
+    
+
 </script>
 
+
 <style>
+.container-breweries {
+  font-family: 'Poppins', sans-serif;
+  font-size: 3vw;
+  text-align: center;
+  background-color: white;
+  display: block;
+  border-radius:3vw;
+  padding: 5vw;
+  justify-content: space-evenly;
+  justify-items: center;
+  align-content: space-evenly;
+  align-items: center;
+  margin-left: 20vw;
+  margin-right: 20vw;
+}
+.brewery-details {
+    font-size: 1.5vw;
+}
+
+.brewery-history {
+    font-style:italic;
+    text-align: left;
+}
+
+.a {
+    padding: 1.5vw;
+}
+
+.logo-image {
+    width: 35vw;
+}
+
 
 </style>
