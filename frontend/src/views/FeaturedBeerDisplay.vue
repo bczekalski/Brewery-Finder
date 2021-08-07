c521f043a9f612f8385c45155cf98374e53a919e
Stefan Krut <skrut710@gmail.com>
2021-08-07 12:17:08 -0400

    big giant cluster***

---
+++ frontend/src/views/FeaturedBeerDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+    <featured-beer />
+
+</template>
+
+<script>
+import FeaturedBeer from '../components/FeaturedBeer.vue'
+export default {
+    name: 'featured-beer-display',
+    components: {
+        FeaturedBeer
+    }
+
+}
+</script>
+
+<style>
+
+</style>
